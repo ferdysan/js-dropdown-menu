edb69e0df1acb8e82b67741d1e7db0ee13109e28
Ferdysan <ferdysan87@gmail.com>
2019-05-09 21:36:58 +0200

    inizio secondo livello

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //attivo o disattivo il menu dropdown_menu
 
-// $('.menu_item').click(function () {
+// $('.element-menu').click(function () {
 //   $(this).toggleClass('cliccato');
 //   $(this).children('.dropdown_menu').toggleClass('aperto');
 // });
@@ -8,17 +8,17 @@
 
 //STESSA COSA CON MOUSE enter
 
-// $('.menu_item').mouseenter(function () {
+// $('.element-menu').mouseenter(function () {
 //   $(this).addClass('apri_menu');
 //   $(this).children('.dropdown_menu').toggle();
 // });
 //
-// $('.menu_item').mouseleave(function() {
+// $('.element-menu').mouseleave(function() {
 //   $(this).removeClass('apri_menu');
 //   $(this).children('dropdown_menu').removeClass('apri_menu');
 // })
 
-$('.menu_item').mouseenter(function() {
+$('.element-menu').mouseenter(function() {
   // quando entro con il mouse in una voce aggiungo la classe apri_menu
   // questa classe mantiene lo stesso stile (colore e sfondo) dell'effetto hover
   // $(this).addClass('apri_menu');
@@ -27,7 +27,7 @@ $('.menu_item').mouseenter(function() {
   $(this).children('.dropdown_menu').addClass('aperto');
 });
 
-$('.menu_item').mouseleave(function() {
+$('.element-menu').mouseleave(function() {
 
   // quando esco con il mouse dall'elemento del menu tolgo la classe apri_menu
   // $(this).removeClass('apri_menu');
